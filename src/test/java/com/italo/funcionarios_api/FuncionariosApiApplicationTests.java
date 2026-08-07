68176ae6b8e396b6771888ed8377ea012331fa42
ITALO COSTA RIBEIRO <italocosta019641@gmail.com>
2026-08-06 22:21:29 -0300

    Initial commit

---
+++ src/test/java/com/italo/funcionarios_api/FuncionariosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.italo.funcionarios_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuncionariosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
