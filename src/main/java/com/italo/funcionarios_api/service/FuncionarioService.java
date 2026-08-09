f5d6f1d87b7c22c4ae4de2249a94e2ecb6349798
ITALO COSTA RIBEIRO <italocosta019641@gmail.com>
2026-08-09 16:01:10 -0300

    feat: Cria repository do funcionario

--- src/main/java/com/italo/funcionarios_api/service/FuncionarioService.java
+++ src/main/java/com/italo/funcionarios_api/service/FuncionarioService.java
@@ -1,4 +1,20 @@
 package com.italo.funcionarios_api.service;
 
+import com.italo.funcionarios_api.model.Funcionario;
+import com.italo.funcionarios_api.repository.FuncionarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class FuncionarioService {
+
+    private final FuncionarioRepository funcionarioRepository;
+
+    public FuncionarioService(FuncionarioRepository funcionarioRepository) {
+        this.funcionarioRepository = funcionarioRepository;
+    }
+
+    public void cadastrarFuncionario(Funcionario funcionario){
+        funcionarioRepository.save(funcionario);
+    }
 }
