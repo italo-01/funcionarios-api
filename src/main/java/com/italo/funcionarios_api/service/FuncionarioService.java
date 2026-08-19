a5b0552c022a5d910f95a0a63e7f73c00c08ccb9
ITALO COSTA RIBEIRO <italocosta019641@gmail.com>
2026-08-18 23:57:57 -0300

    Refactor: Foi feito uma coversão de tipos.
    Foi feita uma conversão de tipo primito boolean para Bolean no switch para verificar se era true false ou null, pois não estava copilando.

--- src/main/java/com/italo/funcionarios_api/service/FuncionarioService.java
+++ src/main/java/com/italo/funcionarios_api/service/FuncionarioService.java
@@ -33,8 +33,8 @@ public class FuncionarioService {
     public List<DadosListagemFuncionario> listarFuncionarios(Boolean ativos) {
         var funcionarios = switch (ativos) {
             case null -> funcionarioRepository.findAll();
-            case true -> funcionarioRepository.findByAtivoTrue();
-            case false -> funcionarioRepository.findByAtivoFalse();
+            case Boolean b when b  -> funcionarioRepository.findByAtivoTrue();
+            case Boolean b -> funcionarioRepository.findByAtivoFalse();
         };
         return funcionarios.stream().map(DadosListagemFuncionario::new).toList();
     }
