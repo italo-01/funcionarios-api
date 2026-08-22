1589c09b439613665ffb956fd4c25189cb920902
ITALO COSTA RIBEIRO <italocosta019641@gmail.com>
2026-08-21 21:47:23 -0300

    feat: cria class TEST Para repository.
    foi criada a class TESTPara repository, para fazer teste unitario com h2 database.

---
+++ src/test/java/com/italo/funcionarios_api/repository/FuncionarioRepositoryTest.java
@@ -0,0 +1,18 @@
+package com.italo.funcionarios_api.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class FuncionarioRepositoryTest {
+
+    @Test
+    void findByAtivoTrue() {
+    }
+
+    @Test
+    void findByAtivoFalse() {
+    }
+}
