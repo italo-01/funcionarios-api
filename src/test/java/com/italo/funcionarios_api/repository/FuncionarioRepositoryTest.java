b594b9f69754f8eb37a2a724b6ab08c112d6b8c9
ITALO COSTA RIBEIRO <italocosta019641@gmail.com>
2026-08-21 22:14:51 -0300

    feat: adiciona o drive do banco de dados em memoria.
    Foi adicioonado as popriedades do bnaco em memoria h2 database nas popriedades para se r usato no teste unitario do repository.

--- src/test/java/com/italo/funcionarios_api/repository/FuncionarioRepositoryTest.java
+++ src/test/java/com/italo/funcionarios_api/repository/FuncionarioRepositoryTest.java
@@ -1,12 +1,21 @@
 package com.italo.funcionarios_api.repository;
 
+import com.italo.funcionarios_api.dto.DadosCadastroFuncionario;
+import com.italo.funcionarios_api.model.Funcionario;
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@ActiveProfiles("test")
 class FuncionarioRepositoryTest {
+
+    @Autowired
+    EntityManager entityManager;
 
     @Test
     void findByAtivoTrue() {
@@ -14,5 +23,11 @@ class FuncionarioRepositoryTest {
 
     @Test
     void findByAtivoFalse() {
+    }
+
+    private Funcionario createFuncionario(DadosCadastroFuncionario dados) {
+        Funcionario funcionario = new Funcionario(dados);
+        this.entityManager.persist(funcionario);
+        return funcionario;
     }
 }
