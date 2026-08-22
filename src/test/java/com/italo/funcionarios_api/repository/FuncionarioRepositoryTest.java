8aace210d58d53b30298236adda7cefcff480abd
ITALO COSTA RIBEIRO <italocosta019641@gmail.com>
2026-08-21 22:51:20 -0300

    feat:Cria corpo do metodo fidByAtivoTrue

--- src/test/java/com/italo/funcionarios_api/repository/FuncionarioRepositoryTest.java
+++ src/test/java/com/italo/funcionarios_api/repository/FuncionarioRepositoryTest.java
@@ -3,6 +3,7 @@ package com.italo.funcionarios_api.repository;
 import com.italo.funcionarios_api.dto.DadosCadastroFuncionario;
 import com.italo.funcionarios_api.model.Funcionario;
 import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
@@ -19,15 +20,24 @@ class FuncionarioRepositoryTest {
 
     @Test
     void findByAtivoTrue() {
+
     }
 
     @Test
-    void findByAtivoFalse() {
+    @DisplayName("should get User sucessfuly from Db")
+    void findByAtivoFalseCase1() {
+        DadosCadastroFuncionario data = new DadosCadastroFuncionario(
+                "Italo",
+                "Dev",
+                java.time.LocalDate.of(1999, 10, 10),
+                new java.math.BigDecimal("1000.00")
+        );
+        this.createFuncionario(data);
     }
 
-    private Funcionario createFuncionario(DadosCadastroFuncionario dados) {
-        Funcionario funcionario = new Funcionario(dados);
-        this.entityManager.persist(funcionario);
-        return funcionario;
+        private Funcionario createFuncionario(DadosCadastroFuncionario dados) {
+            Funcionario funcionario = new Funcionario(dados);
+            this.entityManager.persist(funcionario);
+            return funcionario;
     }
 }
