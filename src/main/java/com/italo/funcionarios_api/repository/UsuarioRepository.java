f58325393fdf382a5307b597eebf6d89f3b344d8
ITALO COSTA RIBEIRO <italocosta019641@gmail.com>
2026-09-06 22:45:08 -0300

    Feat: Cria a Interface UsuarioRepository
    foi criado a Interface UsuarioRepository para poder inserir dados no banco de dados do Usuario.

---
+++ src/main/java/com/italo/funcionarios_api/repository/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package com.italo.funcionarios_api.repository;
+
+import com.italo.funcionarios_api.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    
+}
