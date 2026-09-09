a67ebb6305eca1ae0f20576641e1094a19a92c20
ITALO COSTA RIBEIRO <italocosta019641@gmail.com>
2026-09-08 23:43:12 -0300

    feat: Cria o metodo para criptografa a senha

--- src/main/java/com/italo/funcionarios_api/security/SecuriyConfig.java
+++ src/main/java/com/italo/funcionarios_api/security/SecuriyConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,5 +22,10 @@ public class SecuriyConfig {
                         .anyRequest().permitAll() // TODO: trocar para .authenticated() quando login/JWT estiverem prontos
                 )
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
