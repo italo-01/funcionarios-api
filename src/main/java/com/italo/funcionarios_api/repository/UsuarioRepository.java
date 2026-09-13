f0d9b84949a21eb3ebe7fca925e4cba8af851a5f
ITALO COSTA RIBEIRO <italocosta019641@gmail.com>
2026-09-13 10:07:57 -0300

    feat: implementa a classe AutenticacaoService
    Implementa a classe AutenticacaoService, responsável por buscar o usuário pelo login durante o processo de autenticação do Spring
    Security, delegando a busca para o UsuarioRepository.

--- src/main/java/com/italo/funcionarios_api/repository/UsuarioRepository.java
+++ src/main/java/com/italo/funcionarios_api/repository/UsuarioRepository.java
@@ -2,7 +2,8 @@ package com.italo.funcionarios_api.repository;
 
 import com.italo.funcionarios_api.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
+    UserDetails findByLogin(String login);
 }
---
+++ src/main/java/com/italo/funcionarios_api/service/AutenticacaoService.java
@@ -0,0 +1,21 @@
+package com.italo.funcionarios_api.service;
+
+import com.italo.funcionarios_api.repository.UsuarioRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class AutenticacaoService implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public AutenticacaoService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return loadUserByUsername(username);
+    }
+
+}
